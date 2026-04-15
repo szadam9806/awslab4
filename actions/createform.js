6b68c9f95b0fa741aa2356e4d4efff40e63a83e2
Adam Szymanski <szadam9806@gmail.com>
2026-04-15 19:08:02 +0200

    Lab4: S3 upload, metadata and digest

--- actions/createform.js
+++ actions/createform.js
@@ -1,4 +1,5 @@
 var util = require("util");
+var os = require("os");
 var helpers = require("../helpers");
 var Policy = require("../s3post").Policy;
 var S3Form = require("../s3post").S3Form;
@@ -6,21 +7,36 @@ var AWS_CONFIG_FILE = "config.json";
 var POLICY_FILE = "policy.json";
 var INDEX_TEMPLATE = "index.ejs";
 
-
 var task = function(request, callback){
-	//1. load configuration
+	// 1. load configuration
 	var awsConfig = helpers.readJSONFile(AWS_CONFIG_FILE);
 	var policyData = helpers.readJSONFile(POLICY_FILE);
 
-	//2. prepare policy
+	// 2. prepare policy
 	var policy = new Policy(policyData);
 
-	//3. generate form fields for S3 POST
+	// 3. generate form fields for S3 POST
 	var s3Form = new S3Form(policy);
-	//4. get bucket name
-	
+	var fields = s3Form.generateS3FormFields();
+	fields = s3Form.addS3CredientalsFields(fields, awsConfig);
 
-	callback(null, {template: INDEX_TEMPLATE, params:{fields:[], bucket:""}});
+	// 4. add metadata fields
+
+	fields.push({ name: "x-amz-meta-host", value: os.hostname() });
+
+	// 5. get bucket name
+	var bucket = policy.getConditionValueByKey("bucket");
+
+	console.log("bucket: " + bucket);
+	console.log("fields: " + util.inspect(fields, false, null));
+
+	callback(null, {
+		template: INDEX_TEMPLATE,
+		params: {
+			fields: fields,
+			bucket: bucket
+		}
+	});
 }
 
 exports.action = task;
--- actions/digest.js
+++ actions/digest.js
@@ -1,23 +1,74 @@
 var os = require("os");
+var AWS = require("aws-sdk");
 var helpers = require("../helpers");
 
+var AWS_CONFIG_FILE = "config.json";
 
+var task = function(request, callback) {
+	var awsConfig = helpers.readJSONFile(AWS_CONFIG_FILE);
 
-var task =  function(request, callback){
+	AWS.config.update({
+		accessKeyId: awsConfig.accessKeyId,
+		secretAccessKey: awsConfig.secretAccessKey,
+		region: awsConfig.region || "us-west-2"
+	});
 
-	var algorithms = request.body.alg ? Object.keys(request.body.alg) : [];
-	var loopCount = request.body.loop ? request.body.loop : 1;
-	var doc = request.body.txt ? request.body.txt : "";
-	
-	
-	helpers.calculateMultiDigest(doc, 
-		algorithms, 
-		function(err, digests) {
-			callback(null, digests.join("<br>") + ("<hr>  <br>Service provided by: " + os.hostname()));	
-		}, 
-		loopCount);
+	var s3 = new AWS.S3();
 
-	
-}
+	var bucket = request.query.bucket;
+	var key = request.query.key;
 
-exports.action = task
+	if (!bucket || !key) {
+		callback(null, "Brak parametrów bucket lub key w redirect z S3.");
+		return;
+	}
+
+	s3.getObject(
+		{
+			Bucket: bucket,
+			Key: key
+		},
+		function(err, data) {
+			if (err) {
+				callback(null, "Błąd pobierania pliku z S3:<br>" + err);
+				return;
+			}
+
+			var doc = data.Body.toString("utf8");
+			var algorithms = ["md5", "sha1", "sha256", "sha512"];
+			var metadata = data.Metadata || {};
+
+			helpers.calculateMultiDigest(
+				doc,
+				algorithms,
+				function(err, digests) {
+					if (err) {
+						callback(null, "Błąd liczenia skrótów: " + err);
+						return;
+					}
+
+					var metadataHtml = Object.keys(metadata).length
+						? Object.keys(metadata)
+								.map(function(k) {
+									return "<b>" + k + ":</b> " + metadata[k];
+								})
+								.join("<br>")
+						: "brak";
+
+					var result =
+						"<h3>Wynik</h3>" +
+						"<b>Bucket:</b> " + bucket + "<br>" +
+						"<b>Key:</b> " + key + "<br>" +
+						"<b>Metadata:</b><br>" + metadataHtml + "<hr>" +
+						digests.join("<br>") +
+						"<hr><br>Service provided by: " + os.hostname();
+
+					callback(null, result);
+				},
+				1
+			);
+		}
+	);
+};
+
+exports.action = task;
